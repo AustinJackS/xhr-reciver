90924e5ef5fe5fa404ae31bdcf318baa7ef94b60
AustinJackS <austinjack404@gmail.com>
2017-09-27 12:57:18 -0400

    Update gtime.js

--- gtime.js
+++ gtime.js
@@ -1,1 +1,1 @@
-var x = new Image();x.src="https://www.mobymax.com/MM/SI/sign_in/index?varUsername=asco3025&varFlashVersion=23.0&varPassword=UCWP38097&varParent=0&SchoolUniqueURL=CT1835&varSIRole=S&varRememberMe=0&_=1481334353913";sGames.timerLogic = function () {};void 0
+sGames.timerLogic = function () {};void 0
